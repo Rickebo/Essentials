f6f0b9b39488e26036a884c6fd8a2b452e5d3f10
Simon Pettersson <rickebo@rickebo.com>
2020-05-08 20:09:39 +0200

    Added support for database-optimized balancetop

--- Essentials/src/com/earth2me/essentials/database/DbUserData.java
+++ Essentials/src/com/earth2me/essentials/database/DbUserData.java
@@ -10,15 +10,23 @@ public class DbUserData
     private static final String moneyColumnName = "money";
     private static final String lastSeenColumnName = "last_seen";
     private static final String dataColumnName = "data";
+    private static final String balanceTopColumnName = "exempt_balancetop";
+    private static final String nameColumnName = "name";
     
     @DatabaseColumn(name = uuidColumnName, type = "CHAR(36) NOT NULL PRIMARY KEY", indexName = "uuid_index")
     private String uuid;
+    
+    @DatabaseColumn(name = nameColumnName, type = "TINYTEXT NOT NULL")
+    private String name;
     
     @DatabaseColumn(name = moneyColumnName, type = "DOUBLE NOT NULL", indexName = "money_index")
     private double money;
     
     @DatabaseColumn(name = lastSeenColumnName, type = "BIGINT UNSIGNED", indexName = "last_seen")
     private long lastSeen;
+    
+    @DatabaseColumn(name = balanceTopColumnName, type = "BOOLEAN NOT NULL")
+    private boolean exemptFromBalanceTop;
     
     @DatabaseColumn(name = dataColumnName, type = "TEXT NOT NULL")
     private String data;
@@ -36,6 +44,16 @@ public class DbUserData
         this.data = data;
     }
     
+    public DbUserData(UUID uuid, double money, long lastSeen, String data, boolean exemptFromBalanceTop, String name)
+    {
+        this.uuid = uuid.toString();
+        this.money = money;
+        this.lastSeen = lastSeen;
+        this.data = data;
+        this.exemptFromBalanceTop = exemptFromBalanceTop;
+        this.name = name;
+    }
+    
     public DbUserData(ResultSet resultSet)
             throws SQLException
     {
@@ -43,6 +61,28 @@ public class DbUserData
         money = resultSet.getDouble(moneyColumnName);
         lastSeen = resultSet.getLong(lastSeenColumnName);
         data = resultSet.getString(dataColumnName);
+        exemptFromBalanceTop = resultSet.getBoolean(balanceTopColumnName);
+        name = resultSet.getString(nameColumnName);
+    }
+    
+    public boolean isExemptFromBalanceTop()
+    {
+        return exemptFromBalanceTop;
+    }
+    
+    public void setExemptFromBalanceTop(boolean exemptFromBalanceTop)
+    {
+        this.exemptFromBalanceTop = exemptFromBalanceTop;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
     }
     
     public String getUuid()
